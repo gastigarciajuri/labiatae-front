012f440b8905039ef5608f38179cb6d53d36d6c3
Gastón Garcia Juri <gastongarciajuri@gmail.com>
2024-05-18 17:21:10 -0300

    Branch langing page

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div>
+    <h1 className=' text-9xl'>labiatae</h1>
+    <p className=' w-7'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum aliquam dolorum autem consectetur, culpa quisquam inventore iste. Ducimus officia voluptatem laborum, quasi, libero, veniam accusantium incidunt eius ipsam atque laboriosam?</p>
+   </div>
   )
 }
 
